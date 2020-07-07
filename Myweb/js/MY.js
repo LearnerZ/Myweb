3431bb53af14c796246700ca25b03a42063e2b27
CCC <Z709394@hotmail.com>
2020-07-07 20:22:48 +0800

    a new floating button

--- Myweb/js/MY.js
+++ Myweb/js/MY.js
@@ -1,10 +1,68 @@
-const person = {
-	firstname: 'John',
-	lastname: 'Doe',
-	age: 30,
-	hobbies:['Music','Science'],
-	address:{
-		strees:''
-	}
-};
-document.getElementById("myfoot-left-column-p").innerHTML="关于我是什么时候开始想做这个网站和我是怎么做这个网站的，到时候我会把我总结出来的路线做成脑图分享出来。有问题的话可以发邮件给我，如果我能帮到忙的话。";
+import { slide as Menu } from 'react-burger-menu'
+
+var styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmItem: {
+    display: 'inline-block'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
+<Menu styles={ styles } />
+
+class Example extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+    .
+    .
+    .
+  }
+
+  render () {
+    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    return (
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+    );
+  }
+}
---
+++ Myweb/js/floatbutton.js
@@ -0,0 +1,45 @@
+    document.body.style.height = document.documentElement.clientHeight - 100 + "px";
+
+    var fm = new FloatModule({
+        animation:'spring-in',
+		theme_color:'#ff832a',
+		theme_content_color:'#fff',
+		margin_li:'0px',
+		draggable:true,
+        btn_config:[{
+            icon:'fa fa-share-alt',
+			title:'弹出',
+			click: fnTest
+        },{
+            icon:'fa fa-weibo',
+            title:'分享到微博',
+            click: fnTest,
+        },{
+            icon:'fa fa-wechat',
+            title:'分享到微信',
+            click: fnTest
+        },{
+            icon:'fa fa-qq',
+            title:'分享到qq',
+            click: fnTest
+        }]
+    });
+
+    function fnTest() {
+        alert("hello world");
+    }
+
+    function fnClickStep() {
+        var index = event.currentTarget.getAttribute("index");
+        var steps = document.getElementsByClassName("step");
+        var contents = document.getElementsByClassName("step-content");
+        for (var i=0; i<contents.length; i++) {
+            if (i == index) {
+                contents[i].style.display = "block";
+                steps[i].classList.add("step-active");
+            } else {
+                contents[i].style.display = "none";
+                steps[i].classList.remove("step-active");
+            }
+        }
+    }
